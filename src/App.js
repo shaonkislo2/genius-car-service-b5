c5819c78942ec1befaef5aef856cd696e1bbbb78
shaonkislo <shaon6340@agmail.com>
2022-11-27 21:06:52 +0600

    63.5 -2

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import About from './Pages/About/About';
-import Home from './Pages/Home/Home/Home';
-import Login from './Pages/Login/Login/Login';
-import Register from './Pages/Login/Register/Register';
-import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
-import Footer from './Pages/Shared/Header/Footer/Footer';
-import Header from './Pages/Shared/Header/Header';
-import NotFound from './Pages/Shared/NotFound/NotFound';
-import Checkout from './Pages/Checkout/Checkout/Checkout';
-import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home/Home";
+import Login from "./Pages/Login/Login/Login";
+import Register from "./Pages/Login/Register/Register";
+import ServiceDetail from "./Pages/ServiceDetail/ServiceDetail";
+import Header from "./Pages/Shared/Header/Header";
+import NotFound from "./Pages/Shared/NotFound/NotFound";
+import Checkout from "./Pages/Checkout/Checkout/Checkout";
+import RequireAuth from "./Pages/Login/RequireAuth/RequireAuth";
+import Footer from "./Pages/Shared/Footer/Footer";
 
 function App() {
   return (
-    <div >
+    <div>
       <Header></Header>
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/home' element={<Home/>}></Route>
-        <Route path='/service/:serviceId' element={<ServiceDetail/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='login' element={<Login/>}></Route>
-        <Route path='register' element={<Register/>}></Route>
-        <Route path='checkout' element={
-          <RequireAuth>
-            <Checkout/>
-          </RequireAuth>
-        }></Route>
-        <Route path='*' element={<NotFound/>}></Route>
-        
-       
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+        ></Route>
+        <Route
+          path="/home"
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+        ></Route>
+        <Route path="/service/:serviceId" element={<ServiceDetail />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="register" element={<Register />}></Route>
+        <Route
+          path="checkout"
+          element={
+            <RequireAuth>
+              <Checkout />
+            </RequireAuth>
+          }
+        ></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
-      <Footer>  </Footer>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import Banner from '../Banner/Banner';
-import Experts from '../Experts/Experts';
-import Services from '../Services/Services';
+import React from "react";
+import Banner from "../Banner/Banner";
+import Experts from "../Experts/Experts";
+import Services from "../Services/Services";
+import { Helmet } from 'react-helmet-async';
+import PageTitle from "../../Shared/PageTitle/PageTitle";
+
 
 const Home = () => {
-    return (
-        <>
-            <Banner></Banner>
-            <Services></Services>
-            <Experts></Experts>
-        </>
-    );
+  return (
+    <>
+     <PageTitle title="Home"></PageTitle>
+      <Banner></Banner>
+      <Services></Services>
+      <Experts></Experts>
+    </>
+  );
 };
 
 export default Home;
